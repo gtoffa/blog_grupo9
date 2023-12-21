ae6001f253329670b3f65a669465e66f4b8a757f
gtoffa <guille.toffa@gmail.com>
2023-12-20 21:37:09 -0300

    se agrega seccion de mensajes

--- apps/acercade/urls.py
+++ apps/acercade/urls.py
@@ -6,5 +6,6 @@ app_name = 'acercade'
 urlpatterns = [
     path('', views.formulario_contacto, name='acercade'),
     path('exito/', views.exito, name='exito'),
+    path('mensajes/', views.mensajes, name='mensajes'),
 ]
 
