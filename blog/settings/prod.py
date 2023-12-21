38c3e9b8e5d6a58ed1e91e0c65caf383b8fde75e
gtoffa <guille.toffa@gmail.com>
2023-12-21 00:02:58 -0300

    se agrega ALLOWED_HOSTS

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gtoffa.pythonanywhere.com']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_grupo9',
+        'NAME': 'gtoffa$blog_grupo9',
         'USER': 'gtoffa',
         'PASSWORD': 'LCAi@Srs4KG3CnM',
         'HOST': 'gtoffa.mysql.pythonanywhere-services.com',
